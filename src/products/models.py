475e97f6e8313b8628955a8db666cd4244f45210
gummigulla <gummi.gunnlaugsson@gmail.com>
2020-05-06 12:14:43 +0000

    updates products models and added data

--- src/products/models.py
+++ src/products/models.py
@@ -1,33 +1,61 @@
 from django.db import models
 
+
 # Create your models here.
-"""These are test models. They will be replaced later (or ignored)."""
-class ProductCategoriesTest(models.Model):
+class ProductCategories(models.Model):
     name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
-class ProductsTest(models.Model):
+
+class ShippingCodes(models.Model):
+    code_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.code_name
+
+
+class ConditionCodes(models.Model):
+    condition = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.condition
+
+
+class Products(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField(max_length=999, blank=True)
-    category = models.ForeignKey(ProductCategoriesTest, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategories, on_delete=models.CASCADE)
+    console = models.CharField(max_length=255)
+    manufacturer = models.CharField(max_length=255)
     price = models.FloatField()
-    on_sale = models.BooleanField()
+    shpping_code = models.ForeignKey(ShippingCodes, on_delete=models.CASCADE)
+    condition = models.ForeignKey(ConditionCodes, on_delete=models.CASCADE)
+    description = models.CharField(max_length=4999, blank=True)
 
     def __str__(self):
         return self.name
 
-class ProductImageTest(models.Model):
-    image = models.CharField(max_length=999)
-    product = models.ForeignKey(ProductsTest, on_delete=models.CASCADE)
+
+class ProductImage(models.Model):
+    images = models.CharField(max_length=999)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.image
+        return self.images
 
-"""Add final models here."""
 
-# Products
-# ProductCategories
-# ProductImages
-# ProductReviews
-# Add more models
+class ProductRating(models.Model):
+    rating = models.FloatField()
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.rating
+
+
+class ProductReview(models.Model):
+    review = models.CharField(max_length=999)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review
--- src/products/views.py
+++ src/products/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from products.models import ProductsTest
+from products.models import Products
 
 
 def index(request):
-    context = {'products': ProductsTest.objects.all().order_by('name')}
+    context = {'products': Products.objects.all().order_by('name')}
     return render(request, 'products/products.html', context)
 
 # /products/1
 def get_product_by_id(request, id):
     return render(request, 'products/product_details.html', {
-        'product': get_object_or_404(ProductsTest, pk=id)
+        'product': get_object_or_404(Products, pk=id)
     })
