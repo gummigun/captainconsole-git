6f060e835f73f6dc248cb71a147a7593297f6f3b
gummigulla <gummi.gunnlaugsson@gmail.com>
2020-05-07 14:30:01 +0000

    Added search capabilities

--- src/products/views.py
+++ src/products/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-from products.models import Products
+from django.http import JsonResponse
+from products.models import Products, ProductImage, ProductCategories
 
 
 def index(request):
-    context = {'products': Products.objects.all().order_by('name')}
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        products = [
+            {
+                'id': x.id,
+                'name': x.name,
+                'category': x.category.name,
+                'console': x.console,
+                'manufacturer': x.manufacturer,
+                'price': x.price,
+                'shipping': x.shpping_code.code_name,
+                'condition': x.condition.condition,
+                'description': x.description,
+                'firstImage': x.productimage_set.first().images,
+            } for x in Products.objects.filter(name__icontains=search_filter)
+        ]
+        return JsonResponse({'data': products})
+    context = {
+        'products': Products.objects.all().order_by('name'),
+    }
     return render(request, 'products/products.html', context)
+    # name = models.CharField(max_length=255)
+    # category = models.ForeignKey(ProductCategories, on_delete=models.CASCADE)
+    # console = models.CharField(max_length=255)
+    # manufacturer = models.CharField(max_length=255)
+    # price = models.FloatField()
+    # shpping_code = models.ForeignKey(ShippingCodes, on_delete=models.CASCADE)
+    # condition = models.ForeignKey(ConditionCodes, on_delete=models.CASCADE)
+    # description = models.CharField(max_length=4999, blank=True)
 
 # /products/1
 def get_product_by_id(request, id):
---
+++ src/static/js/products.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('#search-btn').on('click', function(e) {
+        console.log('Button pressed');
+        e.preventDefault();
+        var searchText = $('#search-box').val();
+        console.log(searchText);
+        $.ajax( {
+            url: '/products?search_filter=' + searchText,
+            type: 'GET',
+            success: function(resp) {
+                var newHtml = resp.data.map(d => {
+                    return `<div class="single-product">
+                                <a href="/products/$(d.id)">
+                                    <img class="product-img" src="${d.firstImage}" alt=""/>
+                                    <h4>Product: ${ d.name }</h4>
+                                    <p>Category: ${ d.category }</p>
+                                    <p>Price: ${ d.price }</p>
+                                    <p>Description: ${ d.description }</p>
+                                </a>
+                            </div>`
+                });
+                $('.products').html(newHtml.join(''));
+                $('#search-box').val('');
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+            }
+        })
+    });
+});
