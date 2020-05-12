10731c04cb1a07d2fd9ae117c84334da94c208ab
gummigulla <gummi.gunnlaugsson@gmail.com>
2020-05-12 20:44:46 +0000

    Changed filtering logic back to jsonreponse instead of rendering

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     # Home page url pattern
     path('', views.index, name="products"),
     path('<int:id>', views.get_product_by_id, name="product_details"),
+    path('search/', views.search, name="search"),
+
 ]
