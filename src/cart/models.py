918df539f63beb6260d05b0fa17184043f6c3970
Natalia Natalia <natalia.potamianou@gmail.com>
2020-05-14 15:11:49 +0000

    fixed cart error

--- src/cart/models.py
+++ src/cart/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 from products.models import Products
 # Create your models here.
-class CartItem(models.Model):
-    cart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE, null=True, Blank=True)
-    products = models.ForeignKey(Products, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-    line_total = models.DecimalField(default=10.99, max_digits=1000, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-
 class ShoppingCart(models.Model):
     #items = models.ManyToManyField(CartItem, null=True, blank=True)
     total = models.DecimalField(max_digits=100, decimal_places=2, default=0.00)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     active = models.BooleanField(default=True)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE, null=True)
+    products = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    line_total = models.DecimalField(default=10.99, max_digits=1000, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
