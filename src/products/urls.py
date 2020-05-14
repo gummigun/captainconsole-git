eeb63cc29960e53134a5e408b7df7deea5e84f89
gummigulla <gummi.gunnlaugsson@gmail.com>
2020-05-14 15:00:38 +0000

    Added the filtering function

--- src/products/urls.py
+++ src/products/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     re_path(r'^accessories/$', views.accessories, name="accessories"),
     re_path(r'^used/$', views.used, name="used"),
     path('<int:id>', views.get_product_by_id, name="product_details"),
-    re_path(r'^search/.*', views.search, name="search"),
     path('', include('django.contrib.auth.urls')),
 ]
